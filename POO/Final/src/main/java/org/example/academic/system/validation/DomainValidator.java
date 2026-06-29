b9f52b5e4ec543481654c93b7e2614098dd7f021
otávio <otaviooli1312@gmail.com>
2026-06-29 02:48:43 -0300

    teste: testando workflow pipeline

--- POO/Final/src/main/java/org/example/academic/system/validation/DomainValidator.java
+++ POO/Final/src/main/java/org/example/academic/system/validation/DomainValidator.java
@@ -21,7 +21,7 @@ public class DomainValidator {
         try (ValidatorFactory factory = Validation.buildDefaultValidatorFactory()) {
             VALIDATOR = factory.getValidator();
         }
-    } // teste
+    } // teste aaa
 
     private DomainValidator() {}
 
